79c3bc5220dcf35c80bd7227cc4811d70732e61c
nandaniya480-BH <govindn.brainerhub@gmail.com>
2023-11-30 19:18:04 +0530

    update

--- Laravel/database/factories/ClientPlanFactory.php
+++ Laravel/database/factories/ClientPlanFactory.php
@@ -21,11 +21,51 @@ class ClientPlanFactory extends Factory
         $description = ['Business workdays', 'from Battery storage of the company', 'Battery storage of the company', 'Week ends and bank holidays ', 'Special schedule for Fridays', 'Special schedule for Fridays', 'Business workdays'];
         $purchase_source = ['HUPX', 'Battery', 'OKTE', 'OKTE', 'HUPX', 'HUPX', 'HUPX'];
 
+        $data = [
+            [
+                'short_name' => 'Workweek',
+                'description' => 'Business workdays',
+                'purchase_source' => 'HUPX'
+            ],
+            [
+                'short_name' => 'FromBattery',
+                'description' => 'from Battery storage of the company',
+                'purchase_source' => 'Battery'
+            ],
+            [
+                'short_name' => 'ToBattery',
+                'description' => 'Battery storage of the company',
+                'purchase_source' => 'OKTE'
+            ],
+            [
+                'short_name' => 'Weekend',
+                'description' => 'Week ends and bank holidays',
+                'purchase_source' => 'OKTE'
+            ],
+            [
+                'short_name' => 'Friday',
+                'description' => 'Special schedule for Fridays',
+                'purchase_source' => 'HUPX'
+            ],
+            [
+                'short_name' => 'Friday',
+                'description' => 'Special schedule for Fridays',
+                'purchase_source' => 'HUPX'
+            ],
+            [
+                'short_name' => 'Workweek',
+                'description' => 'Business workdays',
+                'purchase_source' => 'HUPX'
+            ],
+        ];
+
+        $randomPlan = $this->faker->randomElement($data);
+
         return [
-            'short_name' => $this->faker->randomElement($short_name),
-            'description' => $this->faker->randomElement($description),
+            'short_name' => $randomPlan['short_name'],
+            'description' => $randomPlan['description'],
             'status' => $this->faker->randomElement([0, 1]),
-            'purchase_source' => $this->faker->randomElement($purchase_source),
+            'purchase_source' => $randomPlan['purchase_source'],
             'source_capacity' => $this->faker->numberBetween(1, 1000),
         ];
     }
