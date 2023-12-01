658cfb9bf8e4e84df3d6d610e0a7782edd2d5a87
nandaniya480-BH <govindn.brainerhub@gmail.com>
2023-12-01 15:05:13 +0530

    update

--- Laravel/database/seeders/RoleSeeder.php
+++ Laravel/database/seeders/RoleSeeder.php
@@ -16,6 +16,7 @@ class RoleSeeder extends Seeder
             'client'
         ];
         $modules = [
+            'client',
             'client_user',
             'consumption_plan'
         ];
